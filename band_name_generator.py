ff8f6083c7a8b613beca97b8924ce243246295d8
latamishra <75287240+latamishra@users.noreply.github.com>
2021-12-28 19:09:11 +0530

    Update band_name_generator.py

--- band_name_generator.py
+++ band_name_generator.py
@@ -1,5 +1,7 @@
-
+#creating greeting for user.
 print("Welcome to the Band Name Generator.")
+#storing city and pet name.
 city = str(input("What's name of the city you grew up in?\n"))
 pet = str(input("What's your pet's name?\n"))
+#combining both city and pet name to create band name. 
 print("Your band name could be "+city+" "+pet)
