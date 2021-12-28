341efeb9353b6ed599f033aeb196b7b85994c0ca
Lata Mishra <dr.latamishra99@gmail.com>
2021-12-28 18:55:46 +0530

    Initial commit

---
+++ band_name_generator.py
@@ -0,0 +1,5 @@
+
+print("Welcome to the Band Name Generator.")
+city = str(input("What's name of the city you grew up in?\n"))
+pet = str(input("What's your pet's name?\n"))
+print("Your band name could be "+city+" "+pet)
